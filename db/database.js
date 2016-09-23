0e283bca20fd78e704eb490dba48db1fef895134
Vaughn Walters <vaughnwalters@gmail.com>
2016-09-23 15:22:58 -0500

    added register user functionality

--- db/database.js
+++ db/database.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 const MONGODB_URL = 'mongodb://localhost:27017/passport-practice';
 
-mongoose.promise = Promise;
+mongoose.Promise = Promise;
 
 // exporting a function that connects to the database
-module.exports = mongoose.connect(MONGODB_URL);
+module.exports.connect = () => mongoose.connect(MONGODB_URL);
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 const { Router } = require('express');
 const router = Router();
+const User = require('../models/user');
 /////////////////////////////////////////
 
 
@@ -15,18 +16,19 @@ router.get('/register', (req, res) => {
   res.render('register');
 });
 
-
-
 router.get('/login', (req, res) => {
   res.render('login');
 });
 
-router.post('/register', (req, res) => {
-
-
-
-});
+router.post('/register', ({body: {email, password}}, res, error) => {
+  User
+  .create({
+      email,
+      password
+    })
+    .then(() => res.redirect('/login'))
+    .catch(console.error);
+})
 /////////////////////////////////////////
-
 
 module.exports = router;
